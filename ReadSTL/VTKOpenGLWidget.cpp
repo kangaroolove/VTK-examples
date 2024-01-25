dd40c4a6cb0ed13624b219f1697c7febc0857b5d
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-01-25 10:51:33 +0800

    Submit read STL file project

---
+++ ReadSTL/VTKOpenGLWidget.cpp
@@ -0,0 +1,43 @@
+#include "VTKOpenGLWidget.h"
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderer.h>
+#include <vtkConeSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkNew.h>
+#include <vtkSTLReader.h>
+
+VTKOpenGLWidget::VTKOpenGLWidget(QWidget* parent)
+    : QVTKOpenGLNativeWidget(parent)
+    , m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New())
+    , m_renderer(vtkSmartPointer<vtkRenderer>::New())
+{
+    initialize();
+    createTestData();
+}
+
+VTKOpenGLWidget::~VTKOpenGLWidget()
+{
+
+}
+
+void VTKOpenGLWidget::initialize()
+{
+    m_renderWindow->AddRenderer(m_renderer);
+    SetRenderWindow(m_renderWindow);
+}
+
+void VTKOpenGLWidget::createTestData()
+{
+    vtkNew<vtkSTLReader> reader;
+    reader->SetFileName("D:/specimen_16g.STL");
+
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(reader->GetOutputPort());
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+
+    m_renderer->AddActor(actor);
+}
