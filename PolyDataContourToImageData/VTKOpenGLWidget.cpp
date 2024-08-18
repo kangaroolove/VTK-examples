37df7cdb8422f2849b160522d35feb060ff0e4bf
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-08-18 11:31:21 +0800

    Show cutter result

--- PolyDataContourToImageData/VTKOpenGLWidget.cpp
+++ PolyDataContourToImageData/VTKOpenGLWidget.cpp
@@ -19,6 +19,10 @@
 #include <vtkInteractorStyleImage.h>
 #include <QDebug>
 #include <vtkCoordinate.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkNamedColors.h>
+#include <vtkProperty.h>
 
 class InteractorStyleImage : public vtkInteractorStyleImage
 {
@@ -40,7 +44,7 @@ public:
 
   void OnMouseMove() override
   {
-    vtkInteractorStyleImage::OnMouseMove();
+    //vtkInteractorStyleImage::OnMouseMove();
     int eventPosition[2] = { 0 };
     this->GetInteractor()->GetEventPosition(eventPosition);
 
@@ -59,6 +63,89 @@ public:
         qDebug()<<"x = "<<worldPosition[0];
         qDebug()<<"y = "<<worldPosition[1];
         qDebug()<<"z = "<<worldPosition[2];
+
+
+  // 3D source sphere
+  vtkNew<vtkSphereSource> sphereSource;
+  sphereSource->SetPhiResolution(30);
+  sphereSource->SetThetaResolution(30);
+  sphereSource->SetCenter(0, 0, 0);
+  sphereSource->SetRadius(2);
+
+  // generate circle by cutting the sphere with an implicit plane
+  // (through its center, axis-aligned)
+
+  vtkNew<vtkCutter> circleCutter;
+  vtkNew<vtkPlane> cutPlane;
+  cutPlane->SetOrigin(0, 0, 0);
+  cutPlane->SetNormal(0, 0, 1);
+  circleCutter->SetCutFunction(cutPlane);
+  circleCutter->SetInputConnection(sphereSource->GetOutputPort());
+  //circleCutter->SetInputData(sphereSource->GetOutput());
+
+  //circleCutter->Update();
+
+  vtkNew<vtkPolyDataMapper> cutterMapper;
+  cutterMapper->SetInputConnection(circleCutter->GetOutputPort());
+  cutterMapper->SetResolveCoincidentTopologyToPolygonOffset();  
+
+  vtkNew<vtkNamedColors> colors;
+  vtkNew<vtkActor> cutterActor;
+  cutterActor->GetProperty()->SetColor(colors->GetColor3d("red").GetData());
+  cutterActor->SetMapper(cutterMapper);
+  cutterActor->SetPosition(worldPosition);
+
+  m_renderer->AddActor(cutterActor);
+
+
+#if 0
+
+  vtkNew<vtkStripper> stripper;
+  stripper->SetInputConnection(circleCutter->GetOutputPort()); // valid circle
+  //stripper->SetInputData(circleCutter->GetOutput());
+  stripper->Update();
+
+  // that's our circle
+  auto circle = stripper->GetOutput();
+
+  // sweep polygonal data (this is the important thing with contours!)
+  vtkNew<vtkLinearExtrusionFilter> extruder;
+  extruder->SetInputData(circle);
+  extruder->SetScaleFactor(1.);
+  extruder->SetExtrusionTypeToVectorExtrusion();
+  extruder->SetVector(0, 0, 1);
+  extruder->Update();
+
+  double origin[3] = { 0 };
+  double spacing[3] = { 0.5 };
+  int extent[6] = { 0, 99, 0, 99, 0, 99};
+
+  // polygonal data --> image stencil:
+  vtkNew<vtkPolyDataToImageStencil> pol2stenc;
+  pol2stenc->SetTolerance(0); // important if extruder->SetVector(0, 0, 1) !!!
+  // pol2stenc->SetInputConnection(extruder->GetOutputPort());
+  pol2stenc->SetInputData(extruder->GetOutput());
+  pol2stenc->SetOutputOrigin(origin);
+  pol2stenc->SetOutputSpacing(spacing);
+  pol2stenc->SetOutputWholeExtent(extent);
+  pol2stenc->Update();
+
+  // cut the corresponding white image and set the background:
+
+    vtkImageData* image = m_imageActor->GetInput();
+
+    vtkNew<vtkImageStencil> imgstenc;
+    imgstenc->SetInputData(image);
+    //imgstenc->SetStencilConnection(pol2stenc->GetOutputPort());
+    imgstenc->SetStencilData(pol2stenc->GetOutput());
+    imgstenc->ReverseStencilOff();
+    imgstenc->SetBackgroundValue(0);
+    imgstenc->Update();
+
+      m_imageActor->SetInputData(imgstenc->GetOutput());
+      #endif
+
+      m_renderWindow->Render();
     }
 
     m_lastEventPosition[0] = eventPosition[0];
@@ -75,11 +162,23 @@ public:
     m_renderer = renderer;
   }
 
+  void setRenderWindow(vtkRenderWindow* renderWindow)
+  {
+    m_renderWindow = renderWindow;
+  }
+
+  void setImageActor(vtkImageActor* vtkImageActor)
+  {
+    m_imageActor = vtkImageActor;
+  }
+
 private:
   int m_lastEventPosition[2] = { 0 };
   bool m_leftButtonPress = false;
   vtkImageData* m_contouringImage = nullptr;
   vtkRenderer* m_renderer = nullptr;
+  vtkRenderWindow* m_renderWindow;
+  vtkImageActor* m_imageActor;
 };
 vtkStandardNewMacro(InteractorStyleImage);
 
@@ -105,73 +204,26 @@ void VTKOpenGLWidget::initialize()
     m_renderWindow->AddRenderer(m_renderer);
     SetRenderWindow(m_renderWindow);
     m_interactorStyle->setRenderer(m_renderer);
+    m_interactorStyle->setRenderWindow(m_renderWindow);
     m_renderWindow->GetInteractor()->SetInteractorStyle(m_interactorStyle);
 }
 
 void VTKOpenGLWidget::createTestData()
 {
-  // 3D source sphere
-  vtkNew<vtkSphereSource> sphereSource;
-  sphereSource->SetPhiResolution(30);
-  sphereSource->SetThetaResolution(30);
-  sphereSource->SetCenter(0, 0, 0);
-  sphereSource->SetRadius(2);
-
-  // generate circle by cutting the sphere with an implicit plane
-  // (through its center, axis-aligned)
-  vtkNew<vtkCutter> circleCutter;
-  circleCutter->SetInputConnection(sphereSource->GetOutputPort());
-  vtkNew<vtkPlane> cutPlane;
-  cutPlane->SetOrigin(sphereSource->GetCenter());
-  cutPlane->SetNormal(0, 0, 1);
-  circleCutter->SetCutFunction(cutPlane);
-
-  vtkNew<vtkStripper> stripper;
-  stripper->SetInputConnection(circleCutter->GetOutputPort()); // valid circle
-  stripper->Update();
-
-  // that's our circle
-  auto circle = stripper->GetOutput();
-
   // prepare the binary image's voxel grid
   vtkNew<vtkImageData> whiteImage;
-  double bounds[6];
-  circle->GetBounds(bounds);
-
   double spacing[3]; // desired volume spacing
   spacing[0] = 0.5;
   spacing[1] = 0.5;
   spacing[2] = 0.5;
   whiteImage->SetSpacing(spacing);
-
-  // compute dimensions
-  // int dim[3];
-  // for (int i = 0; i < 3; i++)
-  // {
-  //   dim[i] = static_cast<int>(
-  //                ceil((bounds[i * 2 + 1] - bounds[i * 2]) / spacing[i])) +
-  //       1;
-  //   if (dim[i] < 1)
-  //     dim[i] = 1;
-  // }
-  //whiteImage->SetDimensions(dim);
   whiteImage->SetExtent(0, 99, 0, 99, 0, 99);
-  double origin[3];
-
-  // NOTE: I am not sure whether or not we had to add some offset!
-  // origin[0] = bounds[0]; // + spacing[0] / 2;
-  // origin[1] = bounds[2]; // + spacing[1] / 2;
-  // origin[2] = bounds[4]; // + spacing[2] / 2;
-
-  origin[0] = 0; // + spacing[0] / 2;
-  origin[1] = 0; // + spacing[1] / 2;
-  origin[2] = 0; // + spacing[2] / 2;
-
+  double origin[3] = { 0 };
   whiteImage->SetOrigin(origin);
   whiteImage->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
 
   // fill the image with foreground voxels:
-  unsigned char inval = 255;
+  unsigned char inval = 0;
   unsigned char outval = 0;
   vtkIdType count = whiteImage->GetNumberOfPoints();
   for (vtkIdType i = 0; i < count; ++i)
@@ -179,38 +231,10 @@ void VTKOpenGLWidget::createTestData()
     whiteImage->GetPointData()->GetScalars()->SetTuple1(i, inval);
   }
 
-  m_interactorStyle->setContouringImage(whiteImage);
-
-
-
-  // sweep polygonal data (this is the important thing with contours!)
-  vtkNew<vtkLinearExtrusionFilter> extruder;
-  extruder->SetInputData(circle);
-  extruder->SetScaleFactor(1.);
-  extruder->SetExtrusionTypeToVectorExtrusion();
-  extruder->SetVector(0, 0, 1);
-  extruder->Update();
-
-  // polygonal data --> image stencil:
-  vtkNew<vtkPolyDataToImageStencil> pol2stenc;
-  pol2stenc->SetTolerance(0); // important if extruder->SetVector(0, 0, 1) !!!
-  pol2stenc->SetInputConnection(extruder->GetOutputPort());
-  pol2stenc->SetOutputOrigin(origin);
-  pol2stenc->SetOutputSpacing(spacing);
-  pol2stenc->SetOutputWholeExtent(whiteImage->GetExtent());
-  pol2stenc->Update();
-
-  // cut the corresponding white image and set the background:
-  vtkNew<vtkImageStencil> imgstenc;
-  imgstenc->SetInputData(whiteImage);
-  imgstenc->SetStencilConnection(pol2stenc->GetOutputPort());
-  imgstenc->ReverseStencilOff();
-  imgstenc->SetBackgroundValue(outval);
-  imgstenc->Update();
-
-
     vtkNew<vtkImageActor> actor;
-    actor->SetInputData(imgstenc->GetOutput());
+    actor->SetInputData(whiteImage);
+
+      m_interactorStyle->setImageActor(actor);
 
     m_renderer->AddViewProp(actor);
 }
