1ac336c14f2e59d372c42e2bce5ab7ef0478b61a
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-10-03 13:25:48 +0800

    Add schedule and force render logic

--- BetterRendering/VTKOpenGLWidget.cpp
+++ BetterRendering/VTKOpenGLWidget.cpp
@@ -6,11 +6,19 @@
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkNew.h>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QTime>
+#include <QPushButton>
+#include <QDebug>
+#include <QHBoxLayout>
 
 VTKOpenGLWidget::VTKOpenGLWidget(QWidget* parent)
     : QVTKOpenGLNativeWidget(parent)
     , m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New())
     , m_renderer(vtkSmartPointer<vtkRenderer>::New())
+    , m_requestTimer(new QTimer(this))
+    , m_MaximumUpdateRate(60)
 {
     initialize();
     createTestData();
@@ -21,8 +29,16 @@ VTKOpenGLWidget::~VTKOpenGLWidget()
 
 }
 
+void VTKOpenGLWidget::timeout()
+{
+    forceRender();
+}
+
 void VTKOpenGLWidget::initialize()
 {
+    m_requestTimer->setSingleShot(true);
+    connect(m_requestTimer, &QTimer::timeout, this, &VTKOpenGLWidget::timeout);
+
     m_renderWindow->AddRenderer(m_renderer);
     SetRenderWindow(m_renderWindow);
 }
@@ -38,4 +54,44 @@ void VTKOpenGLWidget::createTestData()
     actor->SetMapper(mapper);
 
     m_renderer->AddActor(actor);
+}
+
+void VTKOpenGLWidget::forceRender()
+{
+    if (!m_requestTime.isValid())
+        return;
+
+    // The timer can be stopped if it hasn't time out yet;
+    m_requestTimer->stop();
+    
+    m_requestTime.invalidate();
+
+    if (!this->isVisible())
+        return;
+
+    this->m_renderWindow->Render();
+}
+
+void VTKOpenGLWidget::scheduleRender()
+{
+    double msecsBeforeRender = 0;
+    // If the MaximumUpdateRate is set to 0 then it indicates that rendering is done next time
+    // the application is idle
+    if (m_MaximumUpdateRate > 0)
+    {
+        msecsBeforeRender = 1000. / m_MaximumUpdateRate;
+    }
+    else if (!m_requestTime.isValid())
+    {
+        m_requestTime.start();
+        m_requestTimer->start(static_cast<int>(msecsBeforeRender));
+    }
+    else if (m_requestTime.elapsed() > msecsBeforeRender)
+    {
+        // The rendering hasn't still be done, but msecsBeforeRender milliseconds
+        // have already been elapsed, it is likely that RequestTimer has already
+        // timed out, but the event queue hasn't been processed yet, rendering is
+        // done now to ensure the desired framerate is respected.
+        this->forceRender();
+    }
 }
--- BetterRendering/VTKOpenGLWidget.h
+++ BetterRendering/VTKOpenGLWidget.h
@@ -2,20 +2,29 @@
 
 #include <QVTKOpenGLNativeWidget.h>
 #include <vtkSmartPointer.h>
+#include <QElapsedTimer>
 
 class vtkGenericOpenGLRenderWindow;
 class vtkRenderer;
+class QTimer;
 
 class VTKOpenGLWidget : public QVTKOpenGLNativeWidget
 {
+    Q_OBJECT
 public:
     VTKOpenGLWidget(QWidget* parent = nullptr);
     ~VTKOpenGLWidget();
-
+private slots:
+    void timeout();
+    void forceRender();
+    void scheduleRender();
 private:
     void initialize();
     void createTestData();
 
     vtkSmartPointer<vtkGenericOpenGLRenderWindow> m_renderWindow;
     vtkSmartPointer<vtkRenderer> m_renderer;
+    QTimer* m_requestTimer;
+    QElapsedTimer m_requestTime;
+    double m_MaximumUpdateRate;
 };
