74aafc2085751eba6a347bd1653825ad196b66c1
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 17:17:09 +0800

    Reformat

--- ReadSTL/VTKOpenGLWidget.cpp
+++ ReadSTL/VTKOpenGLWidget.cpp
@@ -1,43 +1,37 @@
 #include "VTKOpenGLWidget.h"
-#include <vtkGenericOpenGLRenderWindow.h>
-#include <vtkSmartPointer.h>
-#include <vtkRenderer.h>
-#include <vtkConeSource.h>
-#include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
+#include <vtkConeSource.h>
+#include <vtkGenericOpenGLRenderWindow.h>
 #include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderer.h>
 #include <vtkSTLReader.h>
+#include <vtkSmartPointer.h>
 
-VTKOpenGLWidget::VTKOpenGLWidget(QWidget* parent)
-    : QVTKOpenGLNativeWidget(parent)
-    , m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New())
-    , m_renderer(vtkSmartPointer<vtkRenderer>::New())
-{
-    initialize();
-    createTestData();
+VTKOpenGLWidget::VTKOpenGLWidget(QWidget *parent)
+    : QVTKOpenGLNativeWidget(parent),
+      m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
+      m_renderer(vtkSmartPointer<vtkRenderer>::New()) {
+  initialize();
+  createTestData();
 }
 
-VTKOpenGLWidget::~VTKOpenGLWidget()
-{
+VTKOpenGLWidget::~VTKOpenGLWidget() {}
 
+void VTKOpenGLWidget::initialize() {
+  m_renderWindow->AddRenderer(m_renderer);
+  SetRenderWindow(m_renderWindow);
 }
 
-void VTKOpenGLWidget::initialize()
-{
-    m_renderWindow->AddRenderer(m_renderer);
-    SetRenderWindow(m_renderWindow);
-}
+void VTKOpenGLWidget::createTestData() {
+  vtkNew<vtkSTLReader> reader;
+  reader->SetFileName("D:/specimen_16g.STL");
 
-void VTKOpenGLWidget::createTestData()
-{
-    vtkNew<vtkSTLReader> reader;
-    reader->SetFileName("D:/specimen_16g.STL");
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(reader->GetOutputPort());
 
-    vtkNew<vtkPolyDataMapper> mapper;
-    mapper->SetInputConnection(reader->GetOutputPort());
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
 
-    vtkNew<vtkActor> actor;
-    actor->SetMapper(mapper);
-
-    m_renderer->AddActor(actor);
+  m_renderer->AddActor(actor);
 }
