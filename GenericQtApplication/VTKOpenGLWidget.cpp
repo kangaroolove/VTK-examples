471fb125a3b8c930a694d14a19b848fe3e47873c
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:19:49 +0800

    Reformat

--- GenericQtApplication/VTKOpenGLWidget.cpp
+++ GenericQtApplication/VTKOpenGLWidget.cpp
@@ -11,25 +11,25 @@ VTKOpenGLWidget::VTKOpenGLWidget(QWidget *parent)
     : QVTKOpenGLNativeWidget(parent),
       m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
       m_renderer(vtkSmartPointer<vtkRenderer>::New()) {
-  initialize();
-  createTestData();
+    initialize();
+    createTestData();
 }
 
 VTKOpenGLWidget::~VTKOpenGLWidget() {}
 
 void VTKOpenGLWidget::initialize() {
-  m_renderWindow->AddRenderer(m_renderer);
-  SetRenderWindow(m_renderWindow);
+    m_renderWindow->AddRenderer(m_renderer);
+    SetRenderWindow(m_renderWindow);
 }
 
 void VTKOpenGLWidget::createTestData() {
-  vtkNew<vtkConeSource> cone;
+    vtkNew<vtkConeSource> cone;
 
-  vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection(cone->GetOutputPort());
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(cone->GetOutputPort());
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
 
-  m_renderer->AddActor(actor);
+    m_renderer->AddActor(actor);
 }
