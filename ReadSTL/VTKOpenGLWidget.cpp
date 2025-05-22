50d92959fa90c1af1c61b5d2c6a7933ffbd7f121
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:31:12 +0800

    Reformat

--- ReadSTL/VTKOpenGLWidget.cpp
+++ ReadSTL/VTKOpenGLWidget.cpp
@@ -12,26 +12,26 @@ VTKOpenGLWidget::VTKOpenGLWidget(QWidget *parent)
     : QVTKOpenGLNativeWidget(parent),
       m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
       m_renderer(vtkSmartPointer<vtkRenderer>::New()) {
-  initialize();
-  createTestData();
+    initialize();
+    createTestData();
 }
 
 VTKOpenGLWidget::~VTKOpenGLWidget() {}
 
 void VTKOpenGLWidget::initialize() {
-  m_renderWindow->AddRenderer(m_renderer);
-  SetRenderWindow(m_renderWindow);
+    m_renderWindow->AddRenderer(m_renderer);
+    SetRenderWindow(m_renderWindow);
 }
 
 void VTKOpenGLWidget::createTestData() {
-  vtkNew<vtkSTLReader> reader;
-  reader->SetFileName("D:/specimen_16g.STL");
+    vtkNew<vtkSTLReader> reader;
+    reader->SetFileName("D:/specimen_16g.STL");
 
-  vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection(reader->GetOutputPort());
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(reader->GetOutputPort());
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
 
-  m_renderer->AddActor(actor);
+    m_renderer->AddActor(actor);
 }
