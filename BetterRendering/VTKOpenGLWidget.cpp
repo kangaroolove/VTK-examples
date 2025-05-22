4085b75d489348711e180601de3221d68d6870b9
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:16:53 +0800

    Reformat

--- BetterRendering/VTKOpenGLWidget.cpp
+++ BetterRendering/VTKOpenGLWidget.cpp
@@ -17,9 +17,10 @@ VTKOpenGLWidget::VTKOpenGLWidget(QWidget *parent)
     : QVTKOpenGLNativeWidget(parent),
       m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
       m_renderer(vtkSmartPointer<vtkRenderer>::New()),
-      m_requestTimer(new QTimer(this)), m_MaximumUpdateRate(60) {
-  initialize();
-  createTestData();
+      m_requestTimer(new QTimer(this)),
+      m_MaximumUpdateRate(60) {
+    initialize();
+    createTestData();
 }
 
 VTKOpenGLWidget::~VTKOpenGLWidget() {}
@@ -27,54 +28,53 @@ VTKOpenGLWidget::~VTKOpenGLWidget() {}
 void VTKOpenGLWidget::timeout() { forceRender(); }
 
 void VTKOpenGLWidget::initialize() {
-  m_requestTimer->setSingleShot(true);
-  connect(m_requestTimer, &QTimer::timeout, this, &VTKOpenGLWidget::timeout);
+    m_requestTimer->setSingleShot(true);
+    connect(m_requestTimer, &QTimer::timeout, this, &VTKOpenGLWidget::timeout);
 
-  m_renderWindow->AddRenderer(m_renderer);
-  SetRenderWindow(m_renderWindow);
+    m_renderWindow->AddRenderer(m_renderer);
+    SetRenderWindow(m_renderWindow);
 }
 
 void VTKOpenGLWidget::createTestData() {
-  vtkNew<vtkConeSource> cone;
+    vtkNew<vtkConeSource> cone;
 
-  vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection(cone->GetOutputPort());
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(cone->GetOutputPort());
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper(mapper);
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
 
-  m_renderer->AddActor(actor);
+    m_renderer->AddActor(actor);
 }
 
 void VTKOpenGLWidget::forceRender() {
-  if (!m_requestTime.isValid())
-    return;
+    if (!m_requestTime.isValid()) return;
 
-  // The timer can be stopped if it hasn't time out yet;
-  m_requestTimer->stop();
+    // The timer can be stopped if it hasn't time out yet;
+    m_requestTimer->stop();
 
-  m_requestTime.invalidate();
+    m_requestTime.invalidate();
 
-  if (!this->isVisible())
-    return;
+    if (!this->isVisible()) return;
 
-  this->m_renderWindow->Render();
+    this->m_renderWindow->Render();
 }
 
 void VTKOpenGLWidget::scheduleRender() {
-  double msecsBeforeRender = 0;
-  // If the MaximumUpdateRate is set to 0 then it indicates that rendering is
-  // done next time the application is idle
-  if (m_MaximumUpdateRate > 0) {
-    msecsBeforeRender = 1000. / m_MaximumUpdateRate;
-  } else if (!m_requestTime.isValid()) {
-    m_requestTime.start();
-    m_requestTimer->start(static_cast<int>(msecsBeforeRender));
-  } else if (m_requestTime.elapsed() > msecsBeforeRender) {
-    // The rendering hasn't still be done, but msecsBeforeRender milliseconds
-    // have already been elapsed, it is likely that RequestTimer has already
-    // timed out, but the event queue hasn't been processed yet, rendering is
-    // done now to ensure the desired framerate is respected.
-    this->forceRender();
-  }
+    double msecsBeforeRender = 0;
+    // If the MaximumUpdateRate is set to 0 then it indicates that rendering is
+    // done next time the application is idle
+    if (m_MaximumUpdateRate > 0) {
+        msecsBeforeRender = 1000. / m_MaximumUpdateRate;
+    } else if (!m_requestTime.isValid()) {
+        m_requestTime.start();
+        m_requestTimer->start(static_cast<int>(msecsBeforeRender));
+    } else if (m_requestTime.elapsed() > msecsBeforeRender) {
+        // The rendering hasn't still be done, but msecsBeforeRender
+        // milliseconds have already been elapsed, it is likely that
+        // RequestTimer has already timed out, but the event queue hasn't been
+        // processed yet, rendering is done now to ensure the desired framerate
+        // is respected.
+        this->forceRender();
+    }
 }
--- BetterRendering/VTKOpenGLWidget.h
+++ BetterRendering/VTKOpenGLWidget.h
@@ -9,22 +9,22 @@ class vtkRenderer;
 class QTimer;
 
 class VTKOpenGLWidget : public QVTKOpenGLNativeWidget {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  VTKOpenGLWidget(QWidget *parent = nullptr);
-  ~VTKOpenGLWidget();
+    VTKOpenGLWidget(QWidget *parent = nullptr);
+    ~VTKOpenGLWidget();
 private slots:
-  void timeout();
-  void forceRender();
-  void scheduleRender();
+    void timeout();
+    void forceRender();
+    void scheduleRender();
 
 private:
-  void initialize();
-  void createTestData();
+    void initialize();
+    void createTestData();
 
-  vtkSmartPointer<vtkGenericOpenGLRenderWindow> m_renderWindow;
-  vtkSmartPointer<vtkRenderer> m_renderer;
-  QTimer *m_requestTimer;
-  QElapsedTimer m_requestTime;
-  double m_MaximumUpdateRate;
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> m_renderWindow;
+    vtkSmartPointer<vtkRenderer> m_renderer;
+    QTimer *m_requestTimer;
+    QElapsedTimer m_requestTime;
+    double m_MaximumUpdateRate;
 };
