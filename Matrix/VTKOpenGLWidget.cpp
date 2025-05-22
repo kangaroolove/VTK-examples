a07702e98576cb0083b4939a842e601d348f19ba
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:28:26 +0800

    Reformat

--- Matrix/VTKOpenGLWidget.cpp
+++ Matrix/VTKOpenGLWidget.cpp
@@ -13,57 +13,59 @@ VTKOpenGLWidget::VTKOpenGLWidget(QWidget *parent)
     : QVTKOpenGLNativeWidget(parent),
       m_renderWindow(vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New()),
       m_renderer(vtkSmartPointer<vtkRenderer>::New()) {
-  initialize();
-  createTestData();
+    initialize();
+    createTestData();
 }
 
 VTKOpenGLWidget::~VTKOpenGLWidget() {}
 
 void VTKOpenGLWidget::initialize() {
-  m_renderWindow->AddRenderer(m_renderer);
-  SetRenderWindow(m_renderWindow);
+    m_renderWindow->AddRenderer(m_renderer);
+    SetRenderWindow(m_renderWindow);
 }
 
 void VTKOpenGLWidget::createTestData() {
-  qDebug() << "Matrix";
-  vtkNew<vtkTransform> transformA;
-  transformA->Translate(10, 0, 0);
-  std::cout << "matrixA=" << std::endl << *transformA->GetMatrix() << std::endl;
+    qDebug() << "Matrix";
+    vtkNew<vtkTransform> transformA;
+    transformA->Translate(10, 0, 0);
+    std::cout << "matrixA=" << std::endl
+              << *transformA->GetMatrix() << std::endl;
 
-  vtkNew<vtkTransform> transformB;
-  transformB->RotateZ(90);
-  std::cout << "matrixB=" << std::endl << *transformB->GetMatrix() << std::endl;
+    vtkNew<vtkTransform> transformB;
+    transformB->RotateZ(90);
+    std::cout << "matrixB=" << std::endl
+              << *transformB->GetMatrix() << std::endl;
 
-  vtkNew<vtkMatrix4x4> matrixC;
-  vtkNew<vtkMatrix4x4> matrixD;
+    vtkNew<vtkMatrix4x4> matrixC;
+    vtkNew<vtkMatrix4x4> matrixD;
 
-  vtkMatrix4x4::Multiply4x4(transformA->GetMatrix(), transformB->GetMatrix(),
-                            matrixC);
-  vtkMatrix4x4::Multiply4x4(transformB->GetMatrix(), transformA->GetMatrix(),
-                            matrixD);
+    vtkMatrix4x4::Multiply4x4(transformA->GetMatrix(), transformB->GetMatrix(),
+                              matrixC);
+    vtkMatrix4x4::Multiply4x4(transformB->GetMatrix(), transformA->GetMatrix(),
+                              matrixD);
 
-  std::cout << "C = A x B" << std::endl << *matrixC << std::endl;
-  std::cout << "D = B x A" << std::endl << *matrixD << std::endl;
+    std::cout << "C = A x B" << std::endl << *matrixC << std::endl;
+    std::cout << "D = B x A" << std::endl << *matrixD << std::endl;
 
-  qDebug() << "pre Multiple";
-  vtkNew<vtkTransform> transformE;
-  transformE->Translate(10, 0, 0);
-  transformE->RotateZ(90);
-  std::cout << "transformE=" << std::endl
-            << *transformE->GetMatrix() << std::endl;
+    qDebug() << "pre Multiple";
+    vtkNew<vtkTransform> transformE;
+    transformE->Translate(10, 0, 0);
+    transformE->RotateZ(90);
+    std::cout << "transformE=" << std::endl
+              << *transformE->GetMatrix() << std::endl;
 
-  qDebug() << "Post multiple";
-  vtkNew<vtkTransform> transformF;
-  transformF->PostMultiply();
-  transformF->Translate(10, 0, 0);
-  transformF->RotateZ(90);
-  std::cout << "transformF=" << std::endl
-            << *transformF->GetMatrix() << std::endl;
+    qDebug() << "Post multiple";
+    vtkNew<vtkTransform> transformF;
+    transformF->PostMultiply();
+    transformF->Translate(10, 0, 0);
+    transformF->RotateZ(90);
+    std::cout << "transformF=" << std::endl
+              << *transformF->GetMatrix() << std::endl;
 
-  qDebug() << "Concatenate";
-  vtkNew<vtkTransform> transformG;
-  transformG->Concatenate(transformA);
-  transformG->Concatenate(transformB);
-  std::cout << "transformG=" << std::endl
-            << *transformG->GetMatrix() << std::endl;
+    qDebug() << "Concatenate";
+    vtkNew<vtkTransform> transformG;
+    transformG->Concatenate(transformA);
+    transformG->Concatenate(transformB);
+    std::cout << "transformG=" << std::endl
+              << *transformG->GetMatrix() << std::endl;
 }
