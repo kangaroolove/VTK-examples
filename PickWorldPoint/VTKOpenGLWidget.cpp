81f4a2ca1b8af4605059db3070e4ed6eb760c2f4
Pang KuanXin <pang.kuanxin@cmsholding.com>
2026-03-24 14:58:44 +0800

    Remove useless functions

--- PickWorldPoint/VTKOpenGLWidget.cpp
+++ PickWorldPoint/VTKOpenGLWidget.cpp
@@ -21,42 +21,6 @@ public:
     vtkTypeMacro(MouseInteractorStyle, vtkInteractorStyleImage);
 
     void setRenderer(vtkRenderer *renderer) { m_renderer = renderer; }
-
-    // void OnMouseMove() override {
-    //     std::array<int, 2> eventPosition;
-    //     this->GetInteractor()->GetEventPosition(eventPosition.data());
-
-    //     qDebug() << "eventPosition " << eventPosition[0] << ", "
-    //              << eventPosition[1];
-
-    //     auto camera = m_renderer->GetActiveCamera();
-    //     std::array<double, 3> focalPoint;
-    //     camera->GetFocalPoint(focalPoint.data());
-
-    //     // std::cout << "camera focalPoint " << focalPoint[0] << ","
-    //     //           << focalPoint[1] << "," << focalPoint[2] << endl;
-
-    //     std::array<double, 3> displayPoint;
-    //     ComputeWorldToDisplay(focalPoint[0], focalPoint[1], focalPoint[2],
-    //                           displayPoint.data());
-
-    //     auto focalDepth = displayPoint[2];
-
-    //     std::array<double, 4> worldPoint;
-    //     ComputeDisplayToWorld(eventPosition[0], eventPosition[1], focalDepth,
-    //                           worldPoint.data());
-
-    //     // std::cout << "worldPoint " << worldPoint[0] << "," << worldPoint[1]
-    //     //           << "," << worldPoint[2] << endl;
-
-    //     vtkNew<vtkCoordinate> coordinate;
-    //     coordinate->SetCoordinateSystemToDisplay();
-    //     coordinate->SetValue(eventPosition[0], eventPosition[1], 0);
-    //     double *worldPosition = coordinate->GetComputedWorldValue(m_renderer);
-    //     // std::cout << "coordinate " << worldPosition[0] << ","
-    //     //           << worldPosition[1] << "," << worldPosition[2] << endl;
-    //     vtkInteractorStyleImage::OnMouseMove();
-    // }
     void OnLeftButtonDown() override {
         std::array<int, 2> eventPosition;
         this->GetInteractor()->GetEventPosition(eventPosition.data());
